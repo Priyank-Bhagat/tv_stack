282ef49c9b1ffd9415ef0691079c60e77c46007f
Priyank-Bhagat <priyankb4dev@proton.me>
2023-10-19 17:45:45 +0530

    added prices in tvmodel

--- lib/model/tv_model.dart
+++ lib/model/tv_model.dart
@@ -1,32 +1,27 @@
-
 import 'dart:math';
 
-double _doubleInRange(Random source, num start, num end) => source.nextDouble() * (end - start) + start;
+double _doubleInRange(Random source, num start, num end) =>
+    source.nextDouble() * (end - start) + start;
 final random = Random();
 
+final tvs = List.generate(
+    _names.length,
+    (index) => TvModel(
+        name: _names[index],
+        image: 'Assets/coffee/${index + 1}.png',
+        price: _prices[index]));
 
-final tvs = List.generate(_names.length, (index)
-=> TvModel(
-    name: _names[index],
-    image: 'Assets/coffee/${index + 1}.png',
-    price: _doubleInRange(random, 3, 7)
-)
-);
-
-class TvModel{
+class TvModel {
   final String name;
   final String image;
   final double price;
 
-
-  TvModel({required this.name,required this.image, required this.price});
+  TvModel({required this.name, required this.image, required this.price});
 }
-
-
 
 final _names = [
   'Samsung S90C',
-  ' TCL QM8 Class',
+  'TCL QM8 Class',
   'Samsung QN95C',
   'Samsung S95C',
   'Sony X90K',
@@ -35,5 +30,19 @@ final _names = [
   'Sony A80K OLED Series',
   'Hisense U8H',
   'LG B2',
-  'TCL 6-Series Roku TV ',
+  'TCL 6-Series Roku TV',
+];
+
+final _prices = [
+  149990.00,
+  818999.00,
+  252490.00,
+  179990.00,
+  379900.00,
+  399990.00,
+  814990.00,
+  699900.00,
+  88353.00,
+  112990.00,
+  219526.00,
 ];
